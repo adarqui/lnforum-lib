ddbafa780888ae3a868816beaa112b05b331afdb
Andrew Darqui <andrew.darqui@gmail.com>
2016-06-06 15:56:28 -0400

    a style change/fix.. need this because lots of threads of 's in them

--- src/LN/Lib/Url.hs
+++ src/LN/Lib/Url.hs
@@ -7,19 +7,28 @@ module LN.Lib.Url (
 
 
 
-import           Data.Char (isAlphaNum)
-import           Data.Text (Text)
-import qualified Data.Text as T
+import           Data.Char             (isAlphaNum)
+import           Data.Text             (Text)
+import qualified Data.Text             as T
+import           Data.Text.ICU.Replace (replaceAll)
 
 
 
 -- | This is used to generate Name's based on DisplayName's, ie a Forum, Board, Thread, or Resource name
 --
--- >> toPrettyUrl  "1 2 3 -- hello world?!bingo-BOOM"
+-- >> toPrettyUrl "1 2 3 -- hello world?!bingo-BOOM"
 -- "1-2-3-hello-world-bingo-boom"
 --
+-- >> toPrettyUrl "ADARQ's Journal"
+-- "adarqs-journal"
+--
 toPrettyUrl :: Text -> Text
-toPrettyUrl = T.intercalate "-" . T.words . T.map (\c -> if not $ isAlphaNum c then ' ' else c) . T.toLower
+toPrettyUrl =
+  T.intercalate "-" .                                   -- replace spaces with dashes
+  T.words .
+  T.map (\c -> if not $ isAlphaNum c then ' ' else c) . -- anything other than alpha numeric becomes a space
+  replaceAll "'s " "s " .                               -- cleans up Ownership's in names
+  T.toLower
 
 
 
