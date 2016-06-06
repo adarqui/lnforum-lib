50f53a83b9026ac23ed94e0797184923ded504ab
Andrew Darqui <andrew.darqui@gmail.com>
2016-06-06 12:54:36 -0400

    toPrettyName

--- src/LN/Lib/Url.hs
+++ src/LN/Lib/Url.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module LN.Lib.Url (
-  toPrettyUrl
+  toPrettyUrl,
+  toPrettyName
 ) where
 
 
@@ -12,10 +13,21 @@ import qualified Data.Text as T
 
 
 
--- | This is used to generate Name's based on DisplayName's
+-- | This is used to generate Name's based on DisplayName's, ie a Forum, Board, Thread, or Resource name
 --
 -- >> toPrettyUrl  "1 2 3 -- hello world?!bingo-BOOM"
 -- "1-2-3-hello-world-bingo-boom"
 --
 toPrettyUrl :: Text -> Text
 toPrettyUrl = T.intercalate "-" . T.words . T.map (\c -> if not $ isAlphaNum c then ' ' else c) . T.toLower
+
+
+
+
+-- | This is used to generate Name's based on DisplayName's, ie an Organization or User name
+--
+-- >> toPrettyName  "Some User Name 123-"
+-- "someusername123"
+--
+toPrettyName :: Text -> Text
+toPrettyName = T.concat . T.words . T.map (\c -> if not $ isAlphaNum c then ' ' else c) . T.toLower
